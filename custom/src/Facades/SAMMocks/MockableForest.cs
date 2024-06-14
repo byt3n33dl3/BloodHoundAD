61bb53fe2c29bae171ae462b4a8e2a17759632fe
p3xsouger <sulabaziz09@gmail.com>
2024-06-14 19:31:38 +0700

    Add files via upload

---
+++ custom/src/Facades/SAMMocks/MockableForest.cs
@@ -0,0 +1,15 @@
+﻿using System.DirectoryServices.ActiveDirectory;
+
+namespace CommonLibTest.Facades
+{
+    public class MockableForest
+    {
+        public static Forest Construct(string forestDnsName)
+        {
+            var forest = FacadeHelpers.GetUninitializedObject<Forest>();
+            FacadeHelpers.SetField(forest, "_forestDnsName", forestDnsName);
+
+            return forest;
+        }
+    }
+}
