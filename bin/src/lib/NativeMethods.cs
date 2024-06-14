8f06dc4b4c6d96286154df3913705bf38702417e
p3xsouger <sulabaziz09@gmail.com>
2024-06-14 07:48:13 +0700

    Add files via upload

---
+++ bin/src/lib/NativeMethods.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using SharpHoundRPC.NetAPINative;
+
+namespace SharpHoundCommonLib
+{
+    [ExcludeFromCodeCoverage]
+    // This class exists entirely as a shim for testing
+    public class NativeMethods
+    {
+        private readonly ILogger _log;
+
+        public NativeMethods(ILogger log = null)
+        {
+            _log = log ?? Logging.LogProvider.CreateLogger("NativeMethods");
+        }
+
+        public NativeMethods()
+        {
+            _log = Logging.LogProvider.CreateLogger("NativeMethods");
+        }
+
+        public virtual NetAPIResult<IEnumerable<NetSessionEnumResults>> NetSessionEnum(string serverName)
+        {
+            return NetAPIMethods.NetSessionEnum(serverName);
+        }
+
+        public virtual NetAPIResult<IEnumerable<NetWkstaUserEnumResults>> NetWkstaUserEnum(string servername)
+        {
+            return NetAPIMethods.NetWkstaUserEnum(servername);
+        }
+
+        public virtual NetAPIResult<NetAPIStructs.DomainControllerInfo> CallDsGetDcName(string computerName,
+            string domainName)
+        {
+            return NetAPIMethods.DsGetDcName(computerName, domainName);
+        }
+    }
+}
